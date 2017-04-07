b76ba9ddcbc441a506962e4e3a5295ad84a0ec94
atif <matifayaz@gmail.com>
2017-04-07 12:57:55 +0500

    Switch Mezzanine to use mysql. Add 127.0.0.1:8000 to ALLOWED_HOSTS

---
+++ myproject/myproject/local_settings.py
@@ -0,0 +1,48 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+DEBUG = True
+
+# Make these unique, and don't share it with anybody.
+SECRET_KEY = "9cqa)_40tn^y87ya=48%@2j6y1q1mv$3*404&jjg4$j#c-s^*8"
+NEVERCACHE_KEY = "na6+2b@py)#k294ux6u43o*%78ark35xd=9kcoyk@+cj$w)air"
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.mysql",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "mezzaninedb",
+        # Not used with sqlite3.
+        "USER": "mezz",
+        # Not used with sqlite3.
+        "PASSWORD": "mezz123",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
+
+###################
+# DEPLOY SETTINGS #
+###################
+
+# Domains for public site
+ALLOWED_HOSTS = [u'127.0.0.1']
+
+# These settings are used by the default fabfile.py provided.
+# Check fabfile.py for defaults.
+
+# FABRIC = {
+#     "DEPLOY_TOOL": "rsync",  # Deploy with "git", "hg", or "rsync"
+#     "SSH_USER": "",  # VPS SSH username
+#     "HOSTS": [""],  # The IP address of your VPS
+#     "DOMAINS": ALLOWED_HOSTS,  # Edit domains in ALLOWED_HOSTS
+#     "REQUIREMENTS_PATH": "requirements.txt",  # Project's pip requirements
+#     "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
+#     "DB_PASS": "",  # Live database password
+#     "ADMIN_PASS": "",  # Live admin user password
+#     "SECRET_KEY": SECRET_KEY,
+#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
+# }
